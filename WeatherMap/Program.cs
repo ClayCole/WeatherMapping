d0fc60ec7d754f3860be1264f8839c299fcb7ccc
Clay Coleman <claycoleman94@gamil.com>
2022-09-12 12:00:15 -0400

    inital upload

---
+++ WeatherMap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using WeatherMap;
+
+
+string key = File.ReadAllText("appsettings.json"); // this read all the text within the appsetting.json
+string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+Console.WriteLine("Hellp, Please enter your Zipcode: ");
+var zipCode = Console.ReadLine();  // want to keep it a string to implement it with the weather app api
+
+//var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIKey}&units=imperial";
+
+Console.WriteLine();
+
+Console.WriteLine($" It is currently {WeatherMapping.GetTemp(apiCall)} degress F in your location.");
---
+++ WeatherMap/WeatherMapping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherMap
+{
+    public class WeatherMapping
+    {
+        public static double GetTemp (string apiCall)
+        {
+            var client = new HttpClient(); // basically  a broswer that can search the web
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+
+        }
+    }
+}
+
